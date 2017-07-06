b4475459b43dd160b812cf739de6b88cb0692626
github username <logicae@users.noreply.github.com>
2017-07-06 14:17:25 -0400

    added css styling and completed fetch syntax

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ReportForm from './components/ReportForm';
 
@@ -15,7 +14,13 @@ constructor() {
 }
 
   componentDidMount() {
-    fetch('/reports')
+    return fetch('/reports', {
+      method: 'GET',
+      headers: {
+        'Accepts': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
       .then(response => response.json())
       .then(reports => this.setState({ reports }))
       .catch(err => console.log(err))
@@ -24,11 +29,25 @@ constructor() {
 
   render() {
     console.log(this.state.reports)
+
     return (
       <div className="App">
         <ReportForm />
+
+        {this.state.reports.map((report) => {
+          return (
+          <div key={report.id} className='box'>
+            <h3>City:</h3> <p>{report.city}</p>
+            <h3>State:</h3> <p>{report.state}</p>
+            <h3>Temperature:</h3> <p>{report.temperature}</p>
+            <h3>Conditions:</h3> <p>{report.conditions}</p>
+          </div>
+        )})}
+
       </div>
-    );
+
+    )
+
   }
 }
 
