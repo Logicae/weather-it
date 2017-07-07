2ef157da214b1b16db760dab33521610f5a2956e
github username <logicae@users.noreply.github.com>
2017-07-07 13:38:48 -0400

    added syntax for redux import and reducers

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import fetchingData from '../reducers/fetchingDataReducer'
+import route from '../reducers/routeReducer'
+import weatherData from '../reducers/weatherDataReducer'
+
+export default createStore(
+    fetchingData,
+    route,
+    applyMiddleware(...middlewares)
+)
 
 const middlewares = [ thunk ]
