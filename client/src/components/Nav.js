7ed4d79ed931531ea03e60be6ff5b1d3f606884b
github username <logicae@users.noreply.github.com>
2017-07-08 13:12:25 -0400

    added components for routes

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'; 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import About from './About'
+import ReportForm from './ReportForm'
+import Weather from './Weather'
+import Home from './Home'
 import '../App.css';
 
 const Nav = () => (
   <Router>
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
+        <button><Link to="/">Home</Link></button>
         <li><Link to="/submit">Submit Local Weather</Link></li>
         <li><Link to="/weather">Weather Stream</Link></li>
-      </ul>
 
       <hr/>
 
       <Route exact path="/" component={Home}/>
-      <Route exact path="/submit" component={Submit}/>
+      <Route exact path="/form" component={ReportForm}/>
       <Route exact path="/weather" component={Weather}/>
       <Route exact path="/about" component={About}/>
     </div>
   </Router>
 )
+
+export default Nav
