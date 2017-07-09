666f068b67dc01d49d2c8da83d2ada623fc8afd9
github username <logicae@users.noreply.github.com>
2017-07-09 00:25:05 -0400

    removed extra syntax

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,20 +2,16 @@ import React, { Component } from 'react';
 import '../App.css';
 import ReportForm from './ReportForm';
 import { connect } from 'react-redux';
-import Home from './Home'
 import Nav from './Nav'
 
 class App extends Component {
 
 constructor() {
   super()
-
-
 }
 
   render() {
     const { routeName } = this.props
-
     return (
       <div className="App">
         <Nav />
