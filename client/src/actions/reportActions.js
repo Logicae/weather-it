a29807f5db5a9c35b7270e062de4e23e8025a2b0
github username <logicae@users.noreply.github.com>
2017-07-09 00:24:30 -0400

    removed extra syntax

--- client/src/actions/reportActions.js
+++ client/src/actions/reportActions.js
@@ -1,18 +1,10 @@
-// export const changeRoute = routeName => {
-//     return {
-//         type: 'CHANGING_ROUTE', 
-//         routeName
-//     }
-// }
+import ReportApi from '../components/ReportApi'
 
-import ReportApi from '../components/ReportApi';
 
 const loadReports = () => {  
   return function(dispatch) {
     return ReportApi.getAllReports().then(reports => {
       dispatch(loadReportsSuccess(reports));
-    }).catch(error => {
-      throw(error);
     })
   }
 }
@@ -22,22 +14,18 @@ const createReport = report => {
     return ReportApi.createReport(report).then(responseReport => {
       dispatch(createReportSuccess(responseReport));
       return responseReport;
-    }).catch(error => {
-      throw(error);
-    });
+    })
   }; 
 }
 
-const loadReportsSuccess = reports => {  
+const loadReportsSuccess = (reports) => {  
   return {type: 'LOAD_REPORTS_SUCCESS', reports};
 }
 
-const createReportSuccess = report => {
+const createReportSuccess = (report) => {
   return {type: 'CREATE_REPORT_SUCCESS', report}
 }
 
-export const LOAD_REPORTS_SUCCESS = 'LOAD_REPORTS_SUCCESS'
-export const CREATE_REPORT_SUCCESS = 'CREATE_REPORT_SUCCESS'
 
 export {
   loadReports,
