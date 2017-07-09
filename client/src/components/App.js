68d5a00ff6ab888ab8db218f86c52b309960436f
github username <logicae@users.noreply.github.com>
2017-07-09 17:32:27 -0400

    changed connect syntax

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from '../actions/reportActions'
 import Nav from './Nav'
 
 class App extends Component {
+componentWillMount() {
+    this.props.actions.loadReports();
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,7 +19,14 @@ class App extends Component {
   }
 }
 
-export default connect(
-    state => ({
-    })
-  , )(App);
+function mapStateToProps(state) {  
+  return {
+    reports: state.reports
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)}
+}
+
+export default connect(mapStateToProps)(App);
