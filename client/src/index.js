8045621daf45fad8f442962f8dc87a7902d14b5a
github username <logicae@users.noreply.github.com>
2017-07-08 20:22:33 -0400

    changed syntax to loadreports

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
+import { loadReports } from './actions/reportActions';
 import store from './store'
 import registerServiceWorker from './registerServiceWorker';
+
+store.dispatch(loadReports());
 
 ReactDOM.render(
     <Provider store={store}>
