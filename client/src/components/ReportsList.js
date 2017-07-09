994e5d6406ca5c8e7f225524fda0159699687eb2
github username <logicae@users.noreply.github.com>
2017-07-09 17:33:35 -0400

    moved map back into file

--- client/src/components/ReportsList.js
+++ client/src/components/ReportsList.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react'; 
+import Report from './Report'
 
-
-const ReportsList = ({reports}) =>  {
-    if (reports) {
-        return(
-            <div>
-                <h2 className = "Weather-Header">Weather Reports</h2>
-                    {reports.map(report => 
-                    <div key={report.id} className='box'>
-                        <h3>City:</h3> <p>{report.city}</p>
-                        <h3>State:</h3> <p>{report.state}</p>
-                        <h3>Temperature:</h3> <p>{report.temperature}</p>
-                        <h3>Conditions:</h3> <p>{report.conditions}</p>
-                    </div>
-                    )}
-            </div>
-        )} else {
-        return(
-            <div> No Reports to List </div>
-        )
-    }
-    }
-    
+function ReportsList({reports}) {
+    return(
+        <div>
+            <h2 className = "Weather-Header">Weather Reports</h2>
+            {console.log({reports})} 
+                {/*{reports.map(report => 
+                <div key={report.id} className='box'>
+                    <h3>City:</h3> <p>{report.city}</p>
+                    <h3>State:</h3> <p>{report.state}</p>
+                    <h3>Temperature:</h3> <p>{report.temperature}</p>
+                    <h3>Conditions:</h3> <p>{report.conditions}</p>
+                </div>
+                )}*/}
+        </div>
+    )
+}
 
 export default ReportsList
