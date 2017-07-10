bc3233f3f99cf97f8c18ca6371558e1140165f1e
github username <logicae@users.noreply.github.com>
2017-07-10 13:18:17 -0400

    added mapping for presentation

--- client/src/components/ReportsList.js
+++ client/src/components/ReportsList.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react'; 
 import Report from './Report'
 
-function ReportsList({reports}) {
+const ReportsList = ({reports}) => {
     return(
         <div>
             <h2 className = "Weather-Header">Weather Reports</h2>
-            {console.log({reports})} 
-                {/*{reports.map(report => 
+                {reports.map(report => 
                 <div key={report.id} className='box'>
                     <h3>City:</h3> <p>{report.city}</p>
                     <h3>State:</h3> <p>{report.state}</p>
                     <h3>Temperature:</h3> <p>{report.temperature}</p>
                     <h3>Conditions:</h3> <p>{report.conditions}</p>
                 </div>
-                )}*/}
+                )}
         </div>
     )
 }
