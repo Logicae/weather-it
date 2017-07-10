570d5ad3cb61479a37b82f15ead9b8503bd71483
github username <logicae@users.noreply.github.com>
2017-07-10 13:16:08 -0400

    moved store into main index file

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { loadReports } from './actions/reportActions';
-import store from './store'
 import registerServiceWorker from './registerServiceWorker';
+import { compose, createStore, applyMiddleware } from 'redux'
+import {autoRehydrate, persistStore} from 'redux-persist'
+import reducers from './reducers/index'
+import thunk from 'redux-thunk'
 
-store.dispatch(loadReports());
+
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    compose(
+        applyMiddleware(thunk),
+        autoRehydrate()
+    )
+)
 
 ReactDOM.render(
     <Provider store={store}>
@@ -15,4 +26,7 @@ ReactDOM.render(
         document.getElementById('root')
     );
 
+store.dispatch(loadReports());
+persistStore(store)
 registerServiceWorker();
+
