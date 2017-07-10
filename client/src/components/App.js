30b4e1ca513c61f11fbe6b4f495b58957313cbe6
github username <logicae@users.noreply.github.com>
2017-07-10 12:56:05 -0400

    removed syntax that is not needed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as actions from '../actions/reportActions'
 import Nav from './Nav'
 
 class App extends Component {
-componentWillMount() {
-    this.props.actions.loadReports();
-  }
-
   render() {
     return (
       <div className="App">
@@ -19,14 +12,4 @@ componentWillMount() {
   }
 }
 
-function mapStateToProps(state) {  
-  return {
-    reports: state.reports
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {actions: bindActionCreators(actions, dispatch)}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
