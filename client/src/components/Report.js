2f2bb380dca9c9e8515c5b3ad841accf38eefc25
github username <logicae@users.noreply.github.com>
2017-07-10 13:17:37 -0400

    function is now working - fixed mapstate - reports to reportsreducer

--- client/src/components/Report.js
+++ client/src/components/Report.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as actions from '../actions/reportActions'
+import * as action from '../actions/reportActions'
+import ReportsList from './ReportsList'
 
 class Report extends Component {
-  componentWillMount() {
-    this.props.actions.loadReports();
-  }
-  
+
   render() {
     const reports = this.props.reports
+    {console.log(reports)}
     return(
     <div>
-      <h2 className = "Weather-Header">Weather Reports</h2>
-      {console.log({reports})} 
+      {this.props.reports && <ReportsList reports={reports}/>}
     </div>
     )}
-}
+  }
+
 
 function mapStateToProps(state) {  
-  return {
-    reports: state.reports
+  if (state.reportsReducer.length > 0) {
+    return {
+      reports: state.reportsReducer
+    }
+  } else {
+    return {
+      reports: [{city: '', state: '', temperature: '', conditions: ''}]
+    }
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {actions: bindActionCreators(actions, dispatch)}
-}
+// function mapDispatchToProps(dispatch) {
+//   return {action: bindActionCreators(action, dispatch)}
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Report);
+export default connect(mapStateToProps)(Report)
 
 // import ReportsList from './ReportsList'
